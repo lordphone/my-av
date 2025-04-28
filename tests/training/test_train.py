105ab1ed518ca1390fc513e4a6cb4263425e067c
lordphone <lordphonewen@gmail.com>
2025-04-28 17:59:28 -0500

    Added statment outputs for training time tracking.

--- tests/training/test_train.py
+++ tests/training/test_train.py
@@ -1,7 +1,10 @@
 import unittest
+import time
 import os
-from src.training.train import train_model
 import torch
+
+from src.training.train import train_model
+from src.data.comma2k19dataset import Comma2k19Dataset
 
 class TestTrainModel(unittest.TestCase):
 
@@ -13,10 +16,16 @@ class TestTrainModel(unittest.TestCase):
     def test_train(self):
         """Test if train_model runs without errors, and check if the model is saved."""
         try:
-            model = train_model(self.dataset_path, window_size=16, batch_size=8, num_epochs=1, lr=0.001)
+            dataset = Comma2k19Dataset(self.dataset_path)
+            dataset_size = len(dataset)
+            print(f"Training on {dataset_size} videos.")
+            start_time = time.time()
+            model = train_model(self.dataset_path, window_size=16, batch_size=2, num_epochs=1, lr=0.001)
             self.assertIsNotNone(model, "Model should not be None after training.")
             saved_model_path = os.path.join(self.dataset_path, 'best_model.pth')
             self.assertTrue(os.path.exists(saved_model_path), "Best model file should be saved.")
+            end_time = time.time()
+            print(f"Training completed in {end_time - start_time:.2f} seconds.")
         except Exception as e:
             self.fail(f"train_model raised an exception: {e}")
 
